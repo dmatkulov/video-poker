9270c61f825c06d9c4b88d3ff64ef21e81957167
Dilshad Matkulov <dilshadmatkulov@gmail.com>
2023-10-21 01:10:46 +0600

    level 2: add class CardDeck

---
+++ src/lib/CardDeck.ts
@@ -0,0 +1,33 @@
+import Card from './Card';
+
+class CardDeck {
+  constructor(
+    public card: Card[] = [],
+    public ranks: (number | string)[] = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'],
+    public suits: string[] = ['diams', 'hearts', 'clubs', 'spades'],
+  ) {
+    suits.forEach(suit => {
+      ranks.forEach(rank => {
+        this.card.push(new Card(rank, suit));
+      });
+    });
+  }
+
+  getCard(): Card {
+    const randomIndex: number = Math.floor(Math.random() * this.card.length);
+    return this.card.splice(randomIndex, 1)[0];
+  }
+
+  getCards(howMany: number) {
+    const cards = [];
+    for (let i = 0; i < howMany; i++) {
+      const card = this.getCard();
+      if (card) {
+        cards.push(card);
+      }
+    }
+    return cards;
+  }
+}
+
+export default CardDeck;
