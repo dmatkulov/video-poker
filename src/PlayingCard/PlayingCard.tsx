7b1a079cc801456bfe50334707d994132b6ff09e
Dilshad Matkulov <dilshadmatkulov@gmail.com>
2023-10-21 01:29:39 +0600

    fix Props in PlayingCard component

--- src/PlayingCard/PlayingCard.tsx
+++ src/PlayingCard/PlayingCard.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
 interface Props {
-  rank: string | number;
-  suit: string;
+  rank: 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 'J' | 'Q' | 'K' | 'A';
+  suit: 'diams' | 'hearts' | 'clubs' | 'spades';
 }
 
 const PlayingCard: React.FC<Props> = ({rank, suit}) => {
   return (
-    <span className="card rank-k diams">
+    <span className={`card rank-${rank} ${suit}`}>
       <span className="rank">{rank}</span>
       <span className="suit">{suit}</span>
     </span>
